91a1eb94f53394d327db155e153ef664a9392936
minhajul-karim <minhajul.kaarim@gmail.com>
2021-03-16 12:25:50 +0600

    Search shows

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-// Default data
-const defaultData = {
-  name: '',
-  summary: '',
-  premiered: '',
-  premiereDate: '',
-  url: '',
-  image: {
-      original: ''
-  },
-  _embedded: {
-      seasons: []
-  }
-}
-
 // Get data
-const getData = async () => {
+const getData = async (showName) => {
   try {
-    const dataResponse = await fetch('https://api.tvmaze.com/singlesearch/shows?q=aj-robibar&embed=seasons')
+    const dataResponse = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${showName}&embed=seasons`)
     if (dataResponse.ok) {
       const data = await dataResponse.json()
       return data
-      // console.log(data)
-      // return data ? data : defaultData
     } else if (dataResponse.status === 404) {
       alert('No show found!')
       return Promise.reject('No show found')
@@ -32,7 +15,6 @@ const getData = async () => {
     throw new Error(err)
   }
 }
-
 
 // Global varible to store show name
 const globalObj = {}
@@ -52,7 +34,7 @@ const saveToGlobal = (name, summary, premiered, premiereDate, url, image) => {
 const drawCard = (data) => {
 	// Grab required variables from data object
 	const { name, summary, premiered, premiereDate, url } = data
-	const image = data.image ? data.image.original : null
+	const image = data.image ? data.image.original : ''
 	// If name is not present in data, use the global name instead
 	saveToGlobal(name, summary, premiered, premiereDate, url, image)
   // Change the page title
@@ -72,37 +54,34 @@ const drawCard = (data) => {
 
 // Function that paints the top navigation
 const drawNav = (data) => {
-  try {
-    const { name } = data
-    const { seasons } = data._embedded
-    // Paint the navigation
-    document.body.innerHTML = `
-      <nav>
-        <a class="brand logo" href="/">${name}</a>
-        <ul>
-          ${seasons.map(season => `
-            <li>
-              <a href="#" class="nav-link" data-season="${season.number}">
-                S${season.number}
-              </a>
-            </li>
-          `).join('')}
-        </ul>
-      </nav>
-      <form id="search-form">
-        <input type="text" placeholder="Your show name here" id="show-name" />
-      </form>
-      <div class="card"></div>
-    `
-  } catch (err) {
-    throw new Error('No data found!')
-  }
+  const { name } = data
+  const { seasons } = data._embedded
+  // Paint the navigation
+  document.body.innerHTML = `
+    <nav>
+      <a class="brand logo" href="/">${name}</a>
+      <ul>
+        ${seasons.map(season => `
+          <li>
+            <a href="#" class="nav-link" data-season="${season.number}">
+              S${season.number}
+            </a>
+          </li>
+        `).join('')}
+      </ul>
+    </nav>
+    <form id="search-form" action="" method="">
+      <input type="text" placeholder="Your show name here" id="show-name" />
+    </form>
+    <div class="card"></div>
+  ` 
 }
 
 // Function that handles click event on top nav
 const navLinkClickHandler = (links, data) => {
 	links.forEach(link => {
 		link.addEventListener('click', (event) => {
+      console.log('clicked')
 			event.preventDefault()
 			const seasonsArr = data._embedded.seasons
 			const season = parseInt(event.target.dataset.season, 10)
@@ -111,21 +90,34 @@ const navLinkClickHandler = (links, data) => {
 	})
 }
 
-// Run the file
-const init = async () => {
-	// Get data
-	const data = await getData()
+// Paint the page
+const drawPage = async (showName = 'sherlock') => {
+  // Get data
+	const data = await getData(showName)
 	// Draw the navigation
 	drawNav(data)
 	// Draw the card
 	drawCard(data)
 	// Handle click events on navigation
 	navLinkClickHandler(document.querySelectorAll('.nav-link'), data)
-  document.getElementById('search-form').addEventListener('submit', (event) => {
+}
+
+// Handle search form submission
+const submitHandler = () => {
+  document.getElementById('search-form').addEventListener('submit', async (event) => {
     event.preventDefault()
     const showName = document.getElementById('show-name').value
-    console.log(showName)
+    await drawPage(showName)
+    submitHandler()
   })
+}
+
+// Run the file
+const init = async () => {
+  // Paint the full page
+  await drawPage()
+  // Show result for show search
+  submitHandler()
 }
 
 init()
